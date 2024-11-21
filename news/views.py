3ded735cf0be22205f6be00add924883552dfd67
jglgap <a23ricardosr@iessanclemente.net>
2024-11-21 14:33:13 +0100

    comienzo configuraciones

--- news/views.py
+++ news/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import News
 # Create your views here.
+def news(request):
+    news = News.objects.all()
+    return render(request,'news/home.html',{'news':news})
